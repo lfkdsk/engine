27da7eb75a4391cd2671d00ee455c84851db1436
liufengkai <liufengkai@bytedance.com>
2019-04-03 15:45:43 +0800

    update upload assets.

--- lfkdsk_build_tools/ios/create-framework.sh
+++ lfkdsk_build_tools/ios/create-framework.sh
@@ -2,7 +2,7 @@
 
 frameworkDir=../frameworks
 
-bundle() {
+function bundle() {
     targetDir=$1
     prefix=$2
     name=$3
@@ -24,6 +24,49 @@ bundle() {
         lipo -create $targetDir/$name.arm64 $targetDir/$name.arm \
             -output $targetDir/$name.framework/$name
     fi
+
+    if [[ ${CIRRUS_RELEASE} == "" ]]; then
+       echo "Not a release. No need to deploy!"
+       exit 0
+    fi
+
+    zip -rjq ${frameworkDir}/${prefix}-artifacts.zip ${targetDir}
+    files=(
+        ${frameworkDir}/$prefix-artifacts.zip
+    )
+    upload_to_assets files
+}
+
+function upload_to_assets() {
+    files_to_upload=$1
+    if [[ ${CIRRUS_RELEASE} == "" ]]; then
+       echo "Not a release. No need to deploy!"
+       exit 0
+    fi
+
+    if [[ ${github_token} != "" ]]; then
+        GITHUB_TOKEN=${github_token}
+    fi
+
+    if [[ ${GITHUB_TOKEN} == "" ]]; then
+      echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
+      exit 1
+    fi
+
+    file_content_type="application/octet-stream"
+    for fpath in $files_to_upload
+    do
+      echo "Uploading $fpath..."
+      name=android-$platform-$mode-$dynamic-$(basename "$fpath")
+      url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+      curl -X POST \
+        --data-binary @$fpath \
+        --header "Authorization: token $GITHUB_TOKEN" \
+        --header "Content-Type: $file_content_type" \
+        $url_to_upload
+    done
+
+    echo uploaded $name
 }
 
 bundle ios_fly_debug ios_fly_release FlyFlutter ios_fly_debug_sim
