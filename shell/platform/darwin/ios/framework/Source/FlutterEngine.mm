c40a47060e892c67f2c04db8c6ca946dea1c8577
Dan Field <dnfield@google.com>
2019-06-25 20:47:55 -0700

    Purge caches on low memory on iOS (#9491)

--- shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
@@ -99,11 +99,21 @@
 
   [self setupChannels];
 
+  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+  [center addObserver:self
+             selector:@selector(onMemoryWarning:)
+                 name:UIApplicationDidReceiveMemoryWarningNotification
+               object:nil];
+
   return self;
 }
 
 - (void)dealloc {
   [_pluginPublications release];
+
+  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+  [center removeObserver:self name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+
   [super dealloc];
 }
 
@@ -742,6 +752,15 @@
   return _weakBinaryMessengerFactory->GetWeakPtr();
 }
 // END
+
+#pragma mark - Memory Notifications
+
+- (void)onMemoryWarning:(NSNotification*)notification {
+  if (_shell) {
+    _shell->NotifyLowMemoryWarning();
+  }
+  [_systemChannel sendMessage:@{@"type" : @"memoryPressure"}];
+}
 
 @end
 
