1106065cbee43352e8c481e3f55be0e0e1694c07
huweijie <huweijie@bytedance.com>
2019-10-21 18:00:59 +0800

    将 ImageLoader 注入逻辑从 FlutterView 抽离

---
+++ shell/platform/android/io/flutter/view/ImageLoaderRegistryImpl.java
@@ -0,0 +1,61 @@
+package flutter.shell.platform.android.io.flutter.view;
+
+import io.flutter.view.AndroidImageLoader.RealImageLoader;
+import io.flutter.embedding.engine.FlutterJNI;
+import io.flutter.view.FlutterNativeView;
+import io.flutter.view.ImageLoaderRegistry;
+
+/**
+ * BD ADD:
+ * ImageLoaderRegistryImpl
+ */
+public class ImageLoaderRegistryImpl implements ImageLoaderRegistry {
+
+    private AndroidImageLoader mAndroidImageLoader;
+    private FlutterNativeView mNativeView;
+
+    public ImageLoaderRegistryImpl(FlutterNativeView nativeView) {
+      mNativeView = nativeView;
+    }
+
+    /**
+     * register android image loader
+     */
+    @Override
+    public void registerImageLoader(RealImageLoader realImageLoader) {
+      ensureAndroidImageLoaderAttached();
+      mAndroidImageLoader.registerImageLoader(realImageLoader);
+    }
+    /**
+     * unregister android image loader
+     */
+    @Override
+    public void unRegisterImageLoader() {
+      mAndroidImageLoader.unRegisterImageLoader();
+    }
+    /**
+     * initialize android image loader
+     */
+    private void ensureAndroidImageLoaderAttached() {
+      if (mAndroidImageLoader != null) {
+        return;
+      }
+
+      mAndroidImageLoader = new AndroidImageLoader();
+      registerAndroidImageLoader(mAndroidImageLoader);
+    }
+    /**
+     * register android image loader
+     */
+    private void registerAndroidImageLoader(AndroidImageLoader androidImageLoader) {
+        mNativeView.getFlutterJNI().registerAndroidImageLoader(androidImageLoader);
+    }
+    /**
+     * unregister android image loader
+     */
+    private void unRegisterAndroidImageLoader() {
+        mNativeView.getFlutterJNI().unRegisterAndroidImageLoader();
+    }
+
+
+}
