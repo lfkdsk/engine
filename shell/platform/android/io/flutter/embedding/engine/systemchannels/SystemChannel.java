458fd686b740290aafbed4176feecb1103ef046d
sunkun <sunkun@bytedance.com>
2019-10-31 15:59:28 +0800

    release vm Android端适配

--- shell/platform/android/io/flutter/embedding/engine/systemchannels/SystemChannel.java
+++ shell/platform/android/io/flutter/embedding/engine/systemchannels/SystemChannel.java
@@ -9,6 +9,8 @@ import android.support.annotation.NonNull;
 import java.util.HashMap;
 import java.util.Map;
 
+// BD ADD
+import io.flutter.embedding.engine.FlutterJNI;
 import io.flutter.embedding.engine.dart.DartExecutor;
 import io.flutter.plugin.common.BasicMessageChannel;
 import io.flutter.plugin.common.JSONMessageCodec;
@@ -20,15 +22,28 @@ public class SystemChannel {
 
   @NonNull
   public final BasicMessageChannel<Object> channel;
+  // BD ADD:START
+  @NonNull
+  private final DartExecutor dartExecutor;
+  // END
 
   public SystemChannel(@NonNull DartExecutor dartExecutor) {
+    // BD ADD
+    this.dartExecutor = dartExecutor;
     this.channel = new BasicMessageChannel<>(dartExecutor, "flutter/system", JSONMessageCodec.INSTANCE);
   }
 
   public void sendMemoryPressureWarning() {
     Map<String, Object> message = new HashMap<>(1);
     message.put("type", "memoryPressure");
-    channel.send(message);
+    // BD MOD
+    //channel.send(message);
+    channel.send(message, new BasicMessageChannel.Reply<Object>() {
+      @Override
+      public void reply(Object reply) {
+        dartExecutor.getFlutterJNI().notifyLowMemory();
+      }
+    });
+    // END
   }
-
 }
