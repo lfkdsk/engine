04b8a2c9c3ea9ed47476c81ba4f960728c412db9
xudong <xudong@bytedance.com>
2020-07-27 03:40:48 +0000

    支持渲染 iOS NV12 格式外接纹理，从而节省大量内存

--- shell/platform/darwin/ios/ios_external_texture_gl.mm
+++ shell/platform/darwin/ios/ios_external_texture_gl.mm
@@ -11,6 +11,10 @@
 #include "flutter/shell/platform/darwin/ios/framework/Source/vsync_waiter_ios.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
+// BD ADD: START xudong
+#include "third_party/skia/include/core/SkYUVAIndex.h"
+#include "third_party/skia/src/gpu/gl/GrGLDefines.h"
+// END
 
 namespace flutter {
 
@@ -38,26 +42,125 @@ void IOSExternalTextureGL::EnsureTextureCacheExists() {
 }
 
 void IOSExternalTextureGL::CreateTextureFromPixelBuffer() {
-  if (buffer_ref_ == nullptr) {
-    return;
-  }
-  CVOpenGLESTextureRef texture;
-  CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
-      kCFAllocatorDefault, cache_ref_, buffer_ref_, nullptr, GL_TEXTURE_2D, GL_RGBA,
-      static_cast<int>(CVPixelBufferGetWidth(buffer_ref_)),
-      static_cast<int>(CVPixelBufferGetHeight(buffer_ref_)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
-      &texture);
-  if (err != noErr) {
-    FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
-  } else {
-    texture_ref_.Reset(texture);
-  }
+    // BD MOD: START xudong
+    if (buffer_ref_ == nullptr) {
+        return;
+    }
+//    if (buffer_ref_ == nullptr) {
+//      return;
+//    }
+//    CVOpenGLESTextureRef texture;
+//    CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
+//        kCFAllocatorDefault, cache_ref_, buffer_ref_, nullptr, GL_TEXTURE_2D, GL_RGBA,
+//        static_cast<int>(CVPixelBufferGetWidth(buffer_ref_)),
+//        static_cast<int>(CVPixelBufferGetHeight(buffer_ref_)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
+//        &texture);
+//    if (err != noErr) {
+//      FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
+//    } else {
+//      texture_ref_.Reset(texture);
+//    }
+
+    if (pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange || pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+        CreateYUVTexturesFromPixelBuffer();
+    } else {
+        CreateRGBATextureFromPixelBuffer();
+    }
 }
+
+void IOSExternalTextureGL::CreateRGBATextureFromPixelBuffer() {
+    CVOpenGLESTextureRef texture;
+    CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
+        kCFAllocatorDefault, cache_ref_, buffer_ref_, nullptr, GL_TEXTURE_2D, GL_RGBA,
+        static_cast<int>(CVPixelBufferGetWidth(buffer_ref_)),
+        static_cast<int>(CVPixelBufferGetHeight(buffer_ref_)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
+        &texture);
+    if (err != noErr) {
+      FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
+    } else {
+      texture_ref_.Reset(texture);
+    }
+}
+
+void IOSExternalTextureGL::CreateYUVTexturesFromPixelBuffer() {
+    size_t width = CVPixelBufferGetWidth(buffer_ref_);
+    size_t height = CVPixelBufferGetHeight(buffer_ref_);
+    {
+        CVOpenGLESTextureRef yTexture;
+        CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
+                                                                    kCFAllocatorDefault, cache_ref_, buffer_ref_, nullptr, GL_TEXTURE_2D, GL_LUMINANCE,
+                                                                    width,
+                                                                    height, GL_LUMINANCE, GL_UNSIGNED_BYTE, 0,
+                                                                    &yTexture);
+        if (err != noErr) {
+            FML_DCHECK(yTexture) << "Could not create texture from pixel buffer: " << err;
+        } else {
+            y_texture_ref_.Reset(yTexture);
+        }
+    }
+    
+    {
+        CVOpenGLESTextureRef uvTexture;
+        CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
+                                                                    kCFAllocatorDefault, cache_ref_, buffer_ref_, nullptr, GL_TEXTURE_2D, GL_LUMINANCE_ALPHA,
+                                                                    width/2,
+                                                                    height/2, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, 1,
+                                                                    &uvTexture);
+        if (err != noErr) {
+            FML_DCHECK(uvTexture) << "Could not create texture from pixel buffer: " << err;
+        } else {
+            uv_texture_ref_.Reset(uvTexture);
+        }
+    }
+}
+
+sk_sp<SkImage> IOSExternalTextureGL::CreateImageFromRGBATexture(GrContext* context, const SkRect& bounds)
+{
+    GrGLTextureInfo textureInfo = {CVOpenGLESTextureGetTarget(texture_ref_),
+                                   CVOpenGLESTextureGetName(texture_ref_), GL_RGBA8_OES};
+    GrBackendTexture backendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, textureInfo);
+    sk_sp<SkImage> image =
+        SkImage::MakeFromTexture(context, backendTexture, kTopLeft_GrSurfaceOrigin,
+                                 kRGBA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+    return image;
+}
+
+sk_sp<SkImage> IOSExternalTextureGL::CreateImageFromYUVTextures(GrContext* context, const SkRect& bounds)
+{
+    GrGLTextureInfo yTextureInfo = {CVOpenGLESTextureGetTarget(y_texture_ref_), CVOpenGLESTextureGetName(y_texture_ref_), GR_GL_LUMINANCE8};
+    GrBackendTexture yBackendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, yTextureInfo);
+    GrGLTextureInfo uvTextureInfo = {CVOpenGLESTextureGetTarget(uv_texture_ref_), CVOpenGLESTextureGetName(uv_texture_ref_), GR_GL_RGBA8};
+    GrBackendTexture uvBackendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, uvTextureInfo);
+    GrBackendTexture nv12TextureHandles[] = {yBackendTexture, uvBackendTexture};
+    SkYUVAIndex yuvaIndices[4] = {
+            SkYUVAIndex{0, SkColorChannel::kR},// Read Y data from the red channel of the first texture
+            SkYUVAIndex{1, SkColorChannel::kR},// Read U data from the red channel of the second texture
+            SkYUVAIndex{1, SkColorChannel::kA},// Read V data from the alpha channel of the second texture, normal NV12 data V should be taken from the green channel, but currently only the uv texture created by GL_LUMINANCE_ALPHA can be used, so the V value is taken from the alpha channel
+            SkYUVAIndex{-1, SkColorChannel::kA}};//-1 means to omit the alpha data of YUVA
+    SkISize size{yBackendTexture.width(), yBackendTexture.height()};
+    sk_sp<SkImage> image = SkImage::MakeFromYUVATextures(context,
+                                                kRec601_SkYUVColorSpace,
+                                                nv12TextureHandles,
+                                                yuvaIndices,
+                                                size,
+                                                kTopLeft_GrSurfaceOrigin,
+                                                nullptr);
+    return image;
+// END
+}
+
 
 bool IOSExternalTextureGL::NeedUpdateTexture(bool freeze) {
   // Update texture if `texture_ref_` is reset to `nullptr` when GrContext
   // is destroied or new frame is ready.
-  return (!freeze && new_frame_ready_) || !texture_ref_;
+    // BD MOD: START xudong
+//return (!freeze && new_frame_ready_) || !texture_ref_;
+  return (!freeze && new_frame_ready_) || !IsTexturesAvailable();
+}
+
+bool IOSExternalTextureGL::IsTexturesAvailable() {
+    return ((pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange  || pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) && (y_texture_ref_ && uv_texture_ref_ )) || (pixelFormat == kCVPixelFormatType_32BGRA && texture_ref_);
+    // END
 }
 
 void IOSExternalTextureGL::Paint(SkCanvas& canvas,
@@ -70,6 +173,9 @@ void IOSExternalTextureGL::Paint(SkCanvas& canvas,
     auto pixelBuffer = [external_texture_.get() copyPixelBuffer];
     if (pixelBuffer) {
       buffer_ref_.Reset(pixelBuffer);
+        // BD ADD: START xudong
+      pixelFormat = CVPixelBufferGetPixelFormatType(buffer_ref_);
+        // END
     }
     CreateTextureFromPixelBuffer();
     new_frame_ready_ = false;
@@ -79,15 +185,27 @@ void IOSExternalTextureGL::Paint(SkCanvas& canvas,
     }
     // END
   }
-  if (!texture_ref_) {
+    // BD MOD: START xudong
+//if (!texture_ref_) {
+  if (!IsTexturesAvailable()) {
+    // END
     return;
   }
-  GrGLTextureInfo textureInfo = {CVOpenGLESTextureGetTarget(texture_ref_),
-                                 CVOpenGLESTextureGetName(texture_ref_), GL_RGBA8_OES};
-  GrBackendTexture backendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, textureInfo);
-  sk_sp<SkImage> image =
-      SkImage::MakeFromTexture(context, backendTexture, kTopLeft_GrSurfaceOrigin,
-                               kRGBA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+    // BD MOD: START xudong
+//    GrGLTextureInfo textureInfo = {CVOpenGLESTextureGetTarget(texture_ref_),
+//                                   CVOpenGLESTextureGetName(texture_ref_), GL_RGBA8_OES};
+//    GrBackendTexture backendTexture(bounds.width(), bounds.height(), GrMipMapped::kNo, textureInfo);
+//    sk_sp<SkImage> image =
+//        SkImage::MakeFromTexture(context, backendTexture, kTopLeft_GrSurfaceOrigin,
+//                                 kRGBA_8888_SkColorType, kPremul_SkAlphaType, nullptr);
+  sk_sp<SkImage> image = nullptr;
+  if (pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange || pixelFormat == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+      image = CreateImageFromYUVTextures(context, bounds);
+  } else {
+      image = CreateImageFromRGBATexture(context, bounds);
+  }
+    // END
+    
   FML_DCHECK(image) << "Failed to create SkImage from Texture.";
   if (image) {
     SkPaint paint;
