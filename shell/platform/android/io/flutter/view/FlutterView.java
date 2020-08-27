2c87aad1125d7495829448b30095ad038d26ab34
zhoutongwei <zhoutongwei@bytedance.com>
2020-08-27 17:14:18 +0800

    fix: Texture Accessibility mode exception fixes migrated to Surface.
    Signed-off-by: sunkun <sunkun@bytedance.com>
    Signed-off-by: zhoutongwei <zhoutongwei@bytedance.com>

--- shell/platform/android/io/flutter/view/FlutterView.java
+++ shell/platform/android/io/flutter/view/FlutterView.java
@@ -428,6 +428,12 @@ public class FlutterView extends SurfaceView
   }
 
   public void destroy() {
+    // BD ADD: START
+    if (mAccessibilityNodeProvider != null) {
+      mAccessibilityNodeProvider.release();
+      mAccessibilityNodeProvider = null;
+    }
+    // END
     if (!isAttached()) return;
 
     getHolder().removeCallback(mSurfaceCallback);
@@ -782,18 +788,19 @@ public class FlutterView extends SurfaceView
   @Override
   protected void onAttachedToWindow() {
     super.onAttachedToWindow();
-
-    PlatformViewsController platformViewsController =
-        getPluginRegistry().getPlatformViewsController();
-    mAccessibilityNodeProvider =
-        new AccessibilityBridge(
-            this,
-            new AccessibilityChannel(dartExecutor, getFlutterNativeView().getFlutterJNI()),
-            (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE),
-            getContext().getContentResolver(),
-            platformViewsController);
-    mAccessibilityNodeProvider.setOnAccessibilityChangeListener(onAccessibilityChangeListener);
-
+    // BD ADD
+    if (mAccessibilityNodeProvider == null) {
+      PlatformViewsController platformViewsController =
+              getPluginRegistry().getPlatformViewsController();
+      mAccessibilityNodeProvider =
+              new AccessibilityBridge(
+                      this,
+                      new AccessibilityChannel(dartExecutor, getFlutterNativeView().getFlutterJNI()),
+                      (AccessibilityManager) getContext().getSystemService(Context.ACCESSIBILITY_SERVICE),
+                      getContext().getContentResolver(),
+                      platformViewsController);
+      mAccessibilityNodeProvider.setOnAccessibilityChangeListener(onAccessibilityChangeListener);
+    }
     resetWillNotDraw(
         mAccessibilityNodeProvider.isAccessibilityEnabled(),
         mAccessibilityNodeProvider.isTouchExplorationEnabled());
@@ -802,7 +809,8 @@ public class FlutterView extends SurfaceView
   @Override
   protected void onDetachedFromWindow() {
     super.onDetachedFromWindow();
-    releaseAccessibilityNodeProvider();
+    // BD DEL: @sunkun@bytedance.com
+    // releaseAccessibilityNodeProvider();
   }
 
   // TODO(mattcarroll): Confer with Ian as to why we need this method. Delete if possible, otherwise
