af21e083ccbbb5f0c72574b73db631a7e6a71ecf
zhoutongwei <zhoutongwei@bytedance.com>
2020-08-28 18:15:02 +0800

    feat: Adaptation of Flutterloader for engine version 1.20
    Signed-off-by: zhoutongwei <zhoutongwei@bytedance.com>

--- shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
+++ shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -64,6 +64,17 @@ public class FlutterLoader {
 
   private boolean initialized = false;
   @Nullable private Settings settings;
+
+  // BD ADD START:
+  public interface SoLoader {
+    void loadLibrary(Context context, String libraryName);
+  }
+
+  public interface MonitorCallback {
+    void onMonitor(String event, long cost);
+  }
+  // END
+
   private long initStartTimestampMillis;
   private FlutterApplicationInfo flutterApplicationInfo;
 
@@ -128,7 +139,13 @@ public class FlutterLoader {
             ResourceExtractor resourceExtractor = initResources(appContext);
 
             if (FlutterInjector.instance().shouldLoadNative()) {
-              System.loadLibrary("flutter");
+              // BD MOD:
+              //System.loadLibrary("flutter");
+              if (settings != null && settings.getSoLoader() != null) {
+                settings.getSoLoader().loadLibrary(appContext, "flutter");
+              } else {
+                System.loadLibrary("flutter");
+              }
             }
 
             // Prefetch the default font manager as soon as possible on a background thread.
@@ -144,6 +161,10 @@ public class FlutterLoader {
 
             if (resourceExtractor != null) {
               resourceExtractor.waitForCompletion();
+              if (settings != null && settings.monitorCallback != null) {
+                settings.monitorCallback.onMonitor("resourceExtractor.waitForCompletion", SystemClock.uptimeMillis() - initStartTimestampMillis);
+              }
+
             }
             return new InitResult(
                 PathUtils.getFilesDir(appContext),
@@ -175,15 +196,25 @@ public class FlutterLoader {
       throw new IllegalStateException(
           "ensureInitializationComplete must be called after startInitialization");
     }
+
     try {
       InitResult result = initResultFuture.get();
 
       List<String> shellArgs = new ArrayList<>();
       shellArgs.add("--icu-symbol-prefix=_binary_icudtl_dat");
 
+      // BD ADD: START
+      String nativeLibraryDir = settings.getNativeLibraryDir();
+      if (nativeLibraryDir == null) {
+        nativeLibraryDir = flutterApplicationInfo.nativeLibraryDir;
+      }
+      // END
+
       shellArgs.add(
           "--icu-native-lib-path="
-              + flutterApplicationInfo.nativeLibraryDir
+              // BD MOD:
+              //+ flutterApplicationInfo.nativeLibraryDir
+              + nativeLibraryDir
               + File.separator
               + DEFAULT_LIBRARY);
       if (args != null) {
@@ -210,7 +241,9 @@ public class FlutterLoader {
             "--"
                 + AOT_SHARED_LIBRARY_NAME
                 + "="
-                + flutterApplicationInfo.nativeLibraryDir
+                // BD MOD:
+                //+ flutterApplicationInfo.nativeLibraryDir
+                + nativeLibraryDir
                 + File.separator
                 + flutterApplicationInfo.aotSharedLibraryName);
       }
@@ -233,6 +266,13 @@ public class FlutterLoader {
 
       long initTimeMillis = SystemClock.uptimeMillis() - initStartTimestampMillis;
 
+      // BD ADD: START
+      if (ResourceExtractor.isX86Device() &&
+              !shellArgs.contains(FlutterShellArgs.ARG_ENABLE_SOFTWARE_RENDERING)) {
+        shellArgs.add(FlutterShellArgs.ARG_ENABLE_SOFTWARE_RENDERING);
+      }
+      // END
+
       if (FlutterInjector.instance().shouldLoadNative()) {
         FlutterJNI.nativeInit(
             applicationContext,
@@ -242,14 +282,13 @@ public class FlutterLoader {
             result.engineCachesPath,
             initTimeMillis);
       }
-      // BD ADD: START
-      if (ResourceExtractor.isX86Device() &&
-              !shellArgs.contains(FlutterShellArgs.ARG_ENABLE_SOFTWARE_RENDERING)) {
-        shellArgs.add(FlutterShellArgs.ARG_ENABLE_SOFTWARE_RENDERING);
-      }
-      // END
 
       initialized = true;
+      // BD ADD:START
+      if (settings.monitorCallback != null) {
+        settings.monitorCallback.onMonitor("EnsureInitialized", SystemClock.uptimeMillis() - initStartTimestampMillis);
+      }
+      // END
     } catch (Exception e) {
       Log.e(TAG, "Flutter initialization failed.", e);
       throw new RuntimeException(e);
@@ -370,6 +409,9 @@ public class FlutterLoader {
   public static class Settings {
     private String logTag;
     // BD ADD START:
+    private String nativeLibraryDir;
+    private SoLoader soLoader;
+    private MonitorCallback monitorCallback;
     private boolean disableLeakVM = false;
 
     public boolean isDisableLeakVM() {
@@ -379,6 +421,26 @@ public class FlutterLoader {
     // 页面退出后，FlutterEngine默认是不销毁VM的，disableLeakVM设置在所有页面退出后销毁VM
     public void disableLeakVM() {
         disableLeakVM = true;
+    }
+
+    public String getNativeLibraryDir() {
+      return nativeLibraryDir;
+    }
+
+    public SoLoader getSoLoader() {
+      return soLoader;
+    }
+
+    public void setNativeLibraryDir(String dir) {
+      nativeLibraryDir = dir;
+    }
+
+    public void setSoLoader(SoLoader loader) {
+      soLoader = loader;
+    }
+
+    public void setMonitorCallback(MonitorCallback monitorCallback) {
+      this.monitorCallback = monitorCallback;
     }
     // END
 
