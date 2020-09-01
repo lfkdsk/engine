c2e232ed8adddb1fddd5af137ba71db24fc1b3f9
linxuebin <linxuebin.01@bytedance.com>
2020-09-01 16:25:25 +0800

    feat: Increase thread priority before engine initialization

--- shell/platform/android/android_shell_holder.cc
+++ shell/platform/android/android_shell_holder.cc
@@ -109,6 +109,30 @@ AndroidShellHolder::AndroidShellHolder(
                                     io_runner         // io
   );
 
+  // BD ADD: START
+  task_runners.GetGPUTaskRunner()->PostTask([]() {
+    // Android describes -8 as "most important display threads, for
+    // compositing the screen and retrieving input events". Conservatively
+    // set the GPU thread to slightly lower priority than it.
+    // BD MOD:
+    // if (::setpriority(PRIO_PROCESS, gettid(), -5) != 0) {
+    if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
+      // Defensive fallback. Depending on the OEM, it may not be possible
+      // to set priority to -5.
+      if (::setpriority(PRIO_PROCESS, gettid(), -2) != 0) {
+        FML_LOG(ERROR) << "Failed to set GPU task runner priority";
+      }
+    }
+  });
+  task_runners.GetUITaskRunner()->PostTask([]() {
+    // BD MOD:
+    // if (::setpriority(PRIO_PROCESS, gettid(), -1) != 0) {
+    if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
+      FML_LOG(ERROR) << "Failed to set UI task runner priority";
+    }
+  });
+  // END
+
   shell_ =
       Shell::Create(task_runners,             // task runners
                     settings_,                // settings
@@ -118,32 +142,32 @@ AndroidShellHolder::AndroidShellHolder(
 
   platform_view_ = weak_platform_view;
   FML_DCHECK(platform_view_);
-
-  is_valid_ = shell_ != nullptr;
-
-  if (is_valid_) {
-    task_runners.GetGPUTaskRunner()->PostTask([]() {
-      // Android describes -8 as "most important display threads, for
-      // compositing the screen and retrieving input events". Conservatively
-      // set the GPU thread to slightly lower priority than it.
-      // BD MOD:
-      // if (::setpriority(PRIO_PROCESS, gettid(), -5) != 0) {
-      if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
-        // Defensive fallback. Depending on the OEM, it may not be possible
-        // to set priority to -5.
-        if (::setpriority(PRIO_PROCESS, gettid(), -2) != 0) {
-          FML_LOG(ERROR) << "Failed to set GPU task runner priority";
-        }
-      }
-    });
-    task_runners.GetUITaskRunner()->PostTask([]() {
-      // BD MOD:
-      // if (::setpriority(PRIO_PROCESS, gettid(), -1) != 0) {
-      if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
-        FML_LOG(ERROR) << "Failed to set UI task runner priority";
-      }
-    });
-  }
+// BD DEL: START
+//  is_valid_ = shell_ != nullptr;
+//  if (is_valid_) {
+//    task_runners.GetGPUTaskRunner()->PostTask([]() {
+//      // Android describes -8 as "most important display threads, for
+//      // compositing the screen and retrieving input events". Conservatively
+//      // set the GPU thread to slightly lower priority than it.
+//      // BD MOD:
+//      // if (::setpriority(PRIO_PROCESS, gettid(), -5) != 0) {
+//      if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
+//        // Defensive fallback. Depending on the OEM, it may not be possible
+//        // to set priority to -5.
+//        if (::setpriority(PRIO_PROCESS, gettid(), -2) != 0) {
+//          FML_LOG(ERROR) << "Failed to set GPU task runner priority";
+//        }
+//      }
+//    });
+//    task_runners.GetUITaskRunner()->PostTask([]() {
+//      // BD MOD:
+//      // if (::setpriority(PRIO_PROCESS, gettid(), -1) != 0) {
+//      if (::setpriority(PRIO_PROCESS, gettid(), -10) != 0) {
+//        FML_LOG(ERROR) << "Failed to set UI task runner priority";
+//      }
+//    });
+//  }
+// END
 }
 
 AndroidShellHolder::~AndroidShellHolder() {
