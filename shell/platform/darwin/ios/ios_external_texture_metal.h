jason-simmons/flutter_engine

iOS external texture supports rendering NV12 pixelbuffer (#20082)

flutter/shell/platform/darwin/ios/ios_external_texture_metal.h
@@ -33,6 +33,7 @@ class IOSExternalTextureMetal final : public Texture {
   std::atomic_bool texture_frame_available_;
   fml::CFRef<CVPixelBufferRef> last_pixel_buffer_;
   sk_sp<SkImage> external_image_;
+  OSType pixel_format_ = 0;
 
   // |Texture|
   void Paint(SkCanvas& canvas,
@@ -55,6 +56,10 @@ class IOSExternalTextureMetal final : public Texture {
 
   sk_sp<SkImage> WrapExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
                                          GrDirectContext* context) const;
+  sk_sp<SkImage> WrapRGBAExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
+                                             GrDirectContext* context) const;
+  sk_sp<SkImage> WrapNV12ExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
+                                             GrDirectContext* context) const;
 
   FML_DISALLOW_COPY_AND_ASSIGN(IOSExternalTextureMetal);
 };

flutter/shell/platform/darwin/ios/ios_external_texture_metal.mm
@@ -5,6 +5,7 @@
 #include "flutter/shell/platform/darwin/ios/ios_external_texture_metal.h"
 
 #include "flutter/fml/logging.h"
+#include "third_party/skia/include/core/SkYUVAIndex.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "third_party/skia/include/gpu/mtl/GrMtlTypes.h"
@@ -35,6 +36,8 @@ void IOSExternalTextureMetal::Paint(SkCanvas& canvas,
     auto pixel_buffer = fml::CFRef<CVPixelBufferRef>([external_texture_ copyPixelBuffer]);
     if (!pixel_buffer) {
       pixel_buffer = std::move(last_pixel_buffer_);
+    } else {
+      pixel_format_ = CVPixelBufferGetPixelFormatType(pixel_buffer);
     }
 
     // If the application told us there was a texture frame available but did not provide one when
@@ -65,21 +68,130 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
     return nullptr;
   }
 
+  sk_sp<SkImage> image = nullptr;
+  if (pixel_format_ == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange ||
+      pixel_format_ == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+    image = WrapNV12ExternalPixelBuffer(pixel_buffer, context);
+  } else {
+    image = WrapRGBAExternalPixelBuffer(pixel_buffer, context);
+  }
+
+  if (!image) {
+    FML_DLOG(ERROR) << "Could not wrap Metal texture as a Skia image.";
+  }
+
+  return image;
+}
+
+sk_sp<SkImage> IOSExternalTextureMetal::WrapNV12ExternalPixelBuffer(
+    fml::CFRef<CVPixelBufferRef> pixel_buffer,
+    GrDirectContext* context) const {
   auto texture_size =
       SkISize::Make(CVPixelBufferGetWidth(pixel_buffer), CVPixelBufferGetHeight(pixel_buffer));
+  CVMetalTextureRef y_metal_texture_raw = nullptr;
+  {
+    auto cv_return =
+        CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                  /*textureCache=*/texture_cache_,
+                                                  /*sourceImage=*/pixel_buffer,
+                                                  /*textureAttributes=*/nullptr,
+                                                  /*pixelFormat=*/MTLPixelFormatR8Unorm,
+                                                  /*width=*/texture_size.width(),
+                                                  /*height=*/texture_size.height(),
+                                                  /*planeIndex=*/0u,
+                                                  /*texture=*/&y_metal_texture_raw);
 
-  CVMetalTextureRef metal_texture_raw = NULL;
+    if (cv_return != kCVReturnSuccess) {
+      FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
+      return nullptr;
+    }
+  }
+
+  CVMetalTextureRef uv_metal_texture_raw = nullptr;
+  {
+    auto cv_return =
+        CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                  /*textureCache=*/texture_cache_,
+                                                  /*sourceImage=*/pixel_buffer,
+                                                  /*textureAttributes=*/nullptr,
+                                                  /*pixelFormat=*/MTLPixelFormatRG8Unorm,
+                                                  /*width=*/texture_size.width() / 2,
+                                                  /*height=*/texture_size.height() / 2,
+                                                  /*planeIndex=*/1u,
+                                                  /*texture=*/&uv_metal_texture_raw);
+
+    if (cv_return != kCVReturnSuccess) {
+      FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
+      return nullptr;
+    }
+  }
+
+  fml::CFRef<CVMetalTextureRef> y_metal_texture(y_metal_texture_raw);
+
+  GrMtlTextureInfo y_skia_texture_info;
+  y_skia_texture_info.fTexture = sk_cf_obj<const void*>{
+      [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(y_metal_texture)) retain]};
+
+  GrBackendTexture y_skia_backend_texture(/*width=*/texture_size.width(),
+                                          /*height=*/texture_size.height(),
+                                          /*mipMapped=*/GrMipMapped ::kNo,
+                                          /*textureInfo=*/y_skia_texture_info);
+
+  fml::CFRef<CVMetalTextureRef> uv_metal_texture(uv_metal_texture_raw);
+
+  GrMtlTextureInfo uv_skia_texture_info;
+  uv_skia_texture_info.fTexture = sk_cf_obj<const void*>{
+      [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(uv_metal_texture)) retain]};
+
+  GrBackendTexture uv_skia_backend_texture(/*width=*/texture_size.width(),
+                                           /*height=*/texture_size.height(),
+                                           /*mipMapped=*/GrMipMapped ::kNo,
+                                           /*textureInfo=*/uv_skia_texture_info);
+  GrBackendTexture nv12TextureHandles[] = {y_skia_backend_texture, uv_skia_backend_texture};
+  SkYUVAIndex yuvaIndices[4] = {
+      SkYUVAIndex{0, SkColorChannel::kR},  // Read Y data from the red channel of the first texture
+      SkYUVAIndex{1, SkColorChannel::kR},  // Read U data from the red channel of the second texture
+      SkYUVAIndex{1,
+                  SkColorChannel::kG},  // Read V data from the green channel of the second texture
+      SkYUVAIndex{-1, SkColorChannel::kA}};  //-1 means to omit the alpha data of YUVA
+
+  struct ImageCaptures {
+    fml::CFRef<CVPixelBufferRef> buffer;
+    fml::CFRef<CVMetalTextureRef> y_texture;
+    fml::CFRef<CVMetalTextureRef> uv_texture;
+  };
+
+  auto captures = std::make_unique<ImageCaptures>();
+  captures->buffer = std::move(pixel_buffer);
+  captures->y_texture = std::move(y_metal_texture);
+  captures->uv_texture = std::move(uv_metal_texture);
+
+  SkImage::TextureReleaseProc release_proc = [](SkImage::ReleaseContext release_context) {
+    auto captures = reinterpret_cast<ImageCaptures*>(release_context);
+    delete captures;
+  };
+  sk_sp<SkImage> image = SkImage::MakeFromYUVATextures(
+      context, kRec601_SkYUVColorSpace, nv12TextureHandles, yuvaIndices, texture_size,
+      kTopLeft_GrSurfaceOrigin, /*imageColorSpace=*/nullptr, release_proc, captures.release());
+  return image;
+}
+
+sk_sp<SkImage> IOSExternalTextureMetal::WrapRGBAExternalPixelBuffer(
+    fml::CFRef<CVPixelBufferRef> pixel_buffer,
+    GrDirectContext* context) const {
+  auto texture_size =
+      SkISize::Make(CVPixelBufferGetWidth(pixel_buffer), CVPixelBufferGetHeight(pixel_buffer));
+  CVMetalTextureRef metal_texture_raw = nullptr;
   auto cv_return =
-      CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault,       // allocator
-                                                texture_cache_,            // texture cache
-                                                pixel_buffer,              // source image
-                                                NULL,                      // texture attributes
-                                                MTLPixelFormatBGRA8Unorm,  // pixel format
-                                                texture_size.width(),      // width
-                                                texture_size.height(),     // height
-                                                0u,                        // plane index
-                                                &metal_texture_raw         // [out] texture
-      );
+      CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                /*textureCache=*/texture_cache_,
+                                                /*sourceImage=*/pixel_buffer,
+                                                /*textureAttributes=*/nullptr,
+                                                /*pixelFormat=*/MTLPixelFormatBGRA8Unorm,
+                                                /*width=*/texture_size.width(),
+                                                /*height=*/texture_size.height(),
+                                                /*planeIndex=*/0u,
+                                                /*texture=*/&metal_texture_raw);
 
   if (cv_return != kCVReturnSuccess) {
     FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
@@ -92,11 +204,10 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
   skia_texture_info.fTexture = sk_cf_obj<const void*>{
       [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(metal_texture)) retain]};
 
-  GrBackendTexture skia_backend_texture(texture_size.width(),   // width
-                                        texture_size.height(),  // height
-                                        GrMipMapped ::kNo,      // mip-mapped
-                                        skia_texture_info       // texture info
-  );
+  GrBackendTexture skia_backend_texture(/*width=*/texture_size.width(),
+                                        /*height=*/texture_size.height(),
+                                        /*mipMapped=*/GrMipMapped ::kNo,
+                                        /*textureInfo=*/skia_texture_info);
 
   struct ImageCaptures {
     fml::CFRef<CVPixelBufferRef> buffer;
@@ -112,21 +223,12 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
     delete captures;
   };
 
-  auto image = SkImage::MakeFromTexture(context,                   // context
-                                        skia_backend_texture,      // backend texture
-                                        kTopLeft_GrSurfaceOrigin,  // origin
-                                        kBGRA_8888_SkColorType,    // color type
-                                        kPremul_SkAlphaType,       // alpha type
-                                        nullptr,                   // color space
-                                        release_proc,              // release proc
-                                        captures.release()         // release context
+  auto image =
+      SkImage::MakeFromTexture(context, skia_backend_texture, kTopLeft_GrSurfaceOrigin,
+                               kBGRA_8888_SkColorType, kPremul_SkAlphaType,
+                               /*imageColorSpace=*/nullptr, release_proc, captures.release()
 
-  );
-
-  if (!image) {
-    FML_DLOG(ERROR) << "Could not wrap Metal texture as a Skia image.";
-  }
-
+      );
   return image;
 }
 

lfkdsk/engine

shell/platform/darwin/ios/ios_external_texture_metal.h
@@ -33,6 +33,7 @@ class IOSExternalTextureMetal final : public Texture {
   std::atomic_bool texture_frame_available_;
   fml::CFRef<CVPixelBufferRef> last_pixel_buffer_;
   sk_sp<SkImage> external_image_;
+  OSType pixel_format_ = 0;
 
   // |Texture|
   void Paint(SkCanvas& canvas,
@@ -55,6 +56,10 @@ class IOSExternalTextureMetal final : public Texture {
 
   sk_sp<SkImage> WrapExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
                                          GrContext* context) const;
+  sk_sp<SkImage> WrapRGBAExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
+                                             GrContext* context) const;
+  sk_sp<SkImage> WrapNV12ExternalPixelBuffer(fml::CFRef<CVPixelBufferRef> pixel_buffer,
+                                             GrContext* context) const;
 
   FML_DISALLOW_COPY_AND_ASSIGN(IOSExternalTextureMetal);
 };

shell/platform/darwin/ios/ios_external_texture_metal.mm
@@ -5,6 +5,7 @@
 #include "flutter/shell/platform/darwin/ios/ios_external_texture_metal.h"
 
 #include "flutter/fml/logging.h"
+#include "third_party/skia/include/core/SkYUVAIndex.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/mtl/GrMtlTypes.h"
 
@@ -34,6 +35,8 @@ void IOSExternalTextureMetal::Paint(SkCanvas& canvas,
     auto pixel_buffer = fml::CFRef<CVPixelBufferRef>([external_texture_ copyPixelBuffer]);
     if (!pixel_buffer) {
       pixel_buffer = std::move(last_pixel_buffer_);
+    } else {
+      pixel_format_ = CVPixelBufferGetPixelFormatType(pixel_buffer);
     }
 
     // If the application told us there was a texture frame available but did not provide one when
@@ -64,21 +67,130 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
     return nullptr;
   }
 
+  sk_sp<SkImage> image = nullptr;
+  if (pixel_format_ == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange ||
+      pixel_format_ == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+    image = WrapNV12ExternalPixelBuffer(pixel_buffer, context);
+  } else {
+    image = WrapRGBAExternalPixelBuffer(pixel_buffer, context);
+  }
+
+  if (!image) {
+    FML_DLOG(ERROR) << "Could not wrap Metal texture as a Skia image.";
+  }
+
+  return image;
+}
+
+sk_sp<SkImage> IOSExternalTextureMetal::WrapNV12ExternalPixelBuffer(
+    fml::CFRef<CVPixelBufferRef> pixel_buffer,
+    GrContext* context) const {
   auto texture_size =
       SkISize::Make(CVPixelBufferGetWidth(pixel_buffer), CVPixelBufferGetHeight(pixel_buffer));
+  CVMetalTextureRef y_metal_texture_raw = nullptr;
+  {
+    auto cv_return =
+        CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                  /*textureCache=*/texture_cache_,
+                                                  /*sourceImage=*/pixel_buffer,
+                                                  /*textureAttributes=*/nullptr,
+                                                  /*pixelFormat=*/MTLPixelFormatR8Unorm,
+                                                  /*width=*/texture_size.width(),
+                                                  /*height=*/texture_size.height(),
+                                                  /*planeIndex=*/0u,
+                                                  /*texture=*/&y_metal_texture_raw);
 
-  CVMetalTextureRef metal_texture_raw = NULL;
+    if (cv_return != kCVReturnSuccess) {
+      FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
+      return nullptr;
+    }
+  }
+
+  CVMetalTextureRef uv_metal_texture_raw = nullptr;
+  {
+    auto cv_return =
+        CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                  /*textureCache=*/texture_cache_,
+                                                  /*sourceImage=*/pixel_buffer,
+                                                  /*textureAttributes=*/nullptr,
+                                                  /*pixelFormat=*/MTLPixelFormatRG8Unorm,
+                                                  /*width=*/texture_size.width() / 2,
+                                                  /*height=*/texture_size.height() / 2,
+                                                  /*planeIndex=*/1u,
+                                                  /*texture=*/&uv_metal_texture_raw);
+
+    if (cv_return != kCVReturnSuccess) {
+      FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
+      return nullptr;
+    }
+  }
+
+  fml::CFRef<CVMetalTextureRef> y_metal_texture(y_metal_texture_raw);
+
+  GrMtlTextureInfo y_skia_texture_info;
+  y_skia_texture_info.fTexture = sk_cf_obj<const void*>{
+      [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(y_metal_texture)) retain]};
+
+  GrBackendTexture y_skia_backend_texture(/*width=*/texture_size.width(),
+                                          /*height=*/texture_size.height(),
+                                          /*mipMapped=*/GrMipMapped ::kNo,
+                                          /*textureInfo=*/y_skia_texture_info);
+
+  fml::CFRef<CVMetalTextureRef> uv_metal_texture(uv_metal_texture_raw);
+
+  GrMtlTextureInfo uv_skia_texture_info;
+  uv_skia_texture_info.fTexture = sk_cf_obj<const void*>{
+      [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(uv_metal_texture)) retain]};
+
+  GrBackendTexture uv_skia_backend_texture(/*width=*/texture_size.width(),
+                                           /*height=*/texture_size.height(),
+                                           /*mipMapped=*/GrMipMapped ::kNo,
+                                           /*textureInfo=*/uv_skia_texture_info);
+  GrBackendTexture nv12TextureHandles[] = {y_skia_backend_texture, uv_skia_backend_texture};
+  SkYUVAIndex yuvaIndices[4] = {
+      SkYUVAIndex{0, SkColorChannel::kR},  // Read Y data from the red channel of the first texture
+      SkYUVAIndex{1, SkColorChannel::kR},  // Read U data from the red channel of the second texture
+      SkYUVAIndex{1,
+                  SkColorChannel::kG},  // Read V data from the green channel of the second texture
+      SkYUVAIndex{-1, SkColorChannel::kA}};  //-1 means to omit the alpha data of YUVA
+
+  struct ImageCaptures {
+    fml::CFRef<CVPixelBufferRef> buffer;
+    fml::CFRef<CVMetalTextureRef> y_texture;
+    fml::CFRef<CVMetalTextureRef> uv_texture;
+  };
+
+  auto captures = std::make_unique<ImageCaptures>();
+  captures->buffer = std::move(pixel_buffer);
+  captures->y_texture = std::move(y_metal_texture);
+  captures->uv_texture = std::move(uv_metal_texture);
+
+  SkImage::TextureReleaseProc release_proc = [](SkImage::ReleaseContext release_context) {
+    auto captures = reinterpret_cast<ImageCaptures*>(release_context);
+    delete captures;
+  };
+  sk_sp<SkImage> image = SkImage::MakeFromYUVATextures(
+      context, kRec601_SkYUVColorSpace, nv12TextureHandles, yuvaIndices, texture_size,
+      kTopLeft_GrSurfaceOrigin, /*imageColorSpace=*/nullptr, release_proc, captures.release());
+  return image;
+}
+
+sk_sp<SkImage> IOSExternalTextureMetal::WrapRGBAExternalPixelBuffer(
+    fml::CFRef<CVPixelBufferRef> pixel_buffer,
+    GrContext* context) const {
+  auto texture_size =
+      SkISize::Make(CVPixelBufferGetWidth(pixel_buffer), CVPixelBufferGetHeight(pixel_buffer));
+  CVMetalTextureRef metal_texture_raw = nullptr;
   auto cv_return =
-      CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault,       // allocator
-                                                texture_cache_,            // texture cache
-                                                pixel_buffer,              // source image
-                                                NULL,                      // texture attributes
-                                                MTLPixelFormatBGRA8Unorm,  // pixel format
-                                                texture_size.width(),      // width
-                                                texture_size.height(),     // height
-                                                0u,                        // plane index
-                                                &metal_texture_raw         // [out] texture
-      );
+      CVMetalTextureCacheCreateTextureFromImage(/*allocator=*/kCFAllocatorDefault,
+                                                /*textureCache=*/texture_cache_,
+                                                /*sourceImage=*/pixel_buffer,
+                                                /*textureAttributes=*/nullptr,
+                                                /*pixelFormat=*/MTLPixelFormatBGRA8Unorm,
+                                                /*width=*/texture_size.width(),
+                                                /*height=*/texture_size.height(),
+                                                /*planeIndex=*/0u,
+                                                /*texture=*/&metal_texture_raw);
 
   if (cv_return != kCVReturnSuccess) {
     FML_DLOG(ERROR) << "Could not create Metal texture from pixel buffer: CVReturn " << cv_return;
@@ -91,11 +203,10 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
   skia_texture_info.fTexture = sk_cf_obj<const void*>{
       [reinterpret_cast<NSObject*>(CVMetalTextureGetTexture(metal_texture)) retain]};
 
-  GrBackendTexture skia_backend_texture(texture_size.width(),   // width
-                                        texture_size.height(),  // height
-                                        GrMipMapped ::kNo,      // mip-mapped
-                                        skia_texture_info       // texture info
-  );
+  GrBackendTexture skia_backend_texture(/*width=*/texture_size.width(),
+                                        /*height=*/texture_size.height(),
+                                        /*mipMapped=*/GrMipMapped ::kNo,
+                                        /*textureInfo=*/skia_texture_info);
 
   struct ImageCaptures {
     fml::CFRef<CVPixelBufferRef> buffer;
@@ -111,21 +222,12 @@ sk_sp<SkImage> IOSExternalTextureMetal::WrapExternalPixelBuffer(
     delete captures;
   };
 
-  auto image = SkImage::MakeFromTexture(context,                   // context
-                                        skia_backend_texture,      // backend texture
-                                        kTopLeft_GrSurfaceOrigin,  // origin
-                                        kBGRA_8888_SkColorType,    // color type
-                                        kPremul_SkAlphaType,       // alpha type
-                                        nullptr,                   // color space
-                                        release_proc,              // release proc
-                                        captures.release()         // release context
+  auto image =
+      SkImage::MakeFromTexture(context, skia_backend_texture, kTopLeft_GrSurfaceOrigin,
+                               kBGRA_8888_SkColorType, kPremul_SkAlphaType,
+                               /*imageColorSpace=*/nullptr, release_proc, captures.release()
 
-  );
-
-  if (!image) {
-    FML_DLOG(ERROR) << "Could not wrap Metal texture as a Skia image.";
-  }
-
+      );
   return image;
 }
 